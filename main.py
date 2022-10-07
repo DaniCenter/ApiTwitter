ee06d9dafa2a4fa5aaac33d8b2b2dbd787a58332
Daniel <vilcadaniel53@gmail.com>
2022-10-06 23:50:05 -0500

    Twetet Model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import date, datetime
+from email.policy import default
 from typing import Optional
 from uuid import UUID
 from pydantic import BaseModel, EmailStr, Field, validator
@@ -14,11 +15,11 @@ class UserBase(BaseModel):
     last_name: str = Field(min_length=1, max_length=20)
     birth_date: Optional[date] = Field(default=None)
 
-    @validator(birth_date)
+    @validator("birth_date")
     def older_than_legal_age(cls, v):
-        todays_date = datetime.date.today()
-        delta = todays_date - v
-        if delta.days / 365 <= 18:
+        today = datetime.today()
+        delta = today.year - v.year
+        if delta <= 18:
             raise ValueError("The age must be over 18")
         else:
             return v
@@ -29,7 +30,11 @@ class UserLogin(UserBase):
 
 
 class Tweet(BaseModel):
-    pass
+    tweet_id: UUID = Field()
+    content: str = Field(max_length=256)
+    create_at: datetime = Field(default=datetime.now())
+    update_at: Optional[datetime] = Field()
+    by: UserBase = Field()
 
 
 @app.get("/")
